b1f31385ca4e6501ebd98c2d843c7eb963e2ae32
zhuolonghao <103546522+zhuolonghao@users.noreply.github.com>
2025-02-03 07:39:42 -0500

    nwe

--- 04_monitoring/JPAM_2025/sp500_earning_sourcce/SPX_components.py
+++ 04_monitoring/JPAM_2025/sp500_earning_sourcce/SPX_components.py
@@ -1,42 +1,141 @@
 import pandas as pd
 import numpy as np
-from scipy.interpolate import interp1d
-
-def impute_missing(row):
-    x = np.array([i for i in range(len(row)) if not np.isnan(row[i])])  # Indices of known values
-    y = np.array([row[i] for i in range(len(row)) if not np.isnan(row[i])])  # Known values
-
-    if len(x) < 2:
-        return row  # Skip if not enough data for interpolation
-
-    f = interp1d(x, y, kind="linear", fill_value="extrapolate")
-
-    for i in range(len(row)):
-        if np.isnan(row[i]):
-            row[i] = f(i)  # Impute missing value using interpolation/extrapolation
-
-    return row
-
 
 # Read the Excel file into a dictionary of dataframes
-file_path = "04_monitoring\JPAM_2025\sp500_earning_sourcce\SPX_components.xlsx"  # Replace with your actual file path
-dfs = pd.read_excel(file_path, sheet_name=None, skiprows=1)  # None reads all sheets into a dictionary
+file_path = "04_monitoring\JPAM_2025\sp500_earning_sourcce\data\SP500_components.xlsx"  # Replace with your actual file path
+dfs = pd.read_excel(file_path, sheet_name=None, skiprows=2)  # None reads all sheets into a dictionary
 
 # Display the sheet names
 print(dfs.keys())
 
+# Access a specific sheet
+eps = dfs['EPS_Q']
+rev = dfs['Rev_Q']
+shr = dfs['Share_Q']
+meta = dfs['meta']
+# Apply the function to each row (excluding the 'Constituents' column)
+eps2 = eps.drop(np.where(eps.iloc[:,1]=='#INVALID COMPANY ID')[0]).set_index('Constituents').astype('float')
+rev2 = rev.drop(np.where(rev.iloc[:,1]=='#INVALID COMPANY ID')[0]).set_index('Constituents').astype('float')
+shr2 = shr.drop(np.where(shr.iloc[:,1]=='#INVALID COMPANY ID')[0]).set_index('Constituents').astype('float')
+meta2 = meta.drop(np.where(meta.iloc[:,1]=='#INVALID COMPANY ID')[0]).set_index('Constituents')
 
+cols = eps2.columns
+sectors = meta2['Sector'].unique()
+output_sec = pd.DataFrame()
+_list = []
+for c in cols:
+    e = eps2[c]
+    r = rev2[c]
+    s = shr2[c]
+    tmp = pd.concat([e, r, s], axis=1).dropna()
+    tmp.columns = ['eps', 'revenue', 'share']
+    tmp['net_income'] = tmp['eps'] * tmp['share']
+    tmp['revenue'] = tmp['revenue'] * 1000
+    tmp2 = tmp.sum()
+    c2 = c[3:]+c[1:3]
+    _list.append([c2, tmp2['net_income'], tmp2['revenue'], tmp2['net_income']/tmp2['revenue'], tmp2['share']])
+    for sec in sectors:
+        tickers_in_sector = meta2[meta2['Sector'].eq(sec)]
+        tmp2 = pd.concat([tickers_in_sector, tmp], axis=1).dropna()
+        tmp2 = tmp2.reset_index()
+        tmp2['Quarter'] = c2
+        tmp2['Index'] = 'SP500'
+        output_sec = pd.concat([output_sec, tmp2])
+columns = ['Quarter', 'Earnings', 'Revenue', 'Net Income Margin', 'Shares']
+output = pd.DataFrame(_list, columns=columns)
+
+
+################################################################################
+
+# Read the Excel file into a dictionary of dataframes
+file_path = "04_monitoring\JPAM_2025\sp500_earning_sourcce\data\SP400_components.xlsx"  # Replace with your actual file path
+dfs = pd.read_excel(file_path, sheet_name=None, skiprows=2)  # None reads all sheets into a dictionary
+
+# Display the sheet names
+print(dfs.keys())
 
 # Access a specific sheet
-eps = dfs['EPS_A'].set_index('Constituents')  # Replace 'Sheet1' with the actual sheet name
-rev = dfs['Rev_A'].set_index('Constituents')  # Replace 'Sheet1' with the actual sheet name
-shr = dfs['Share_A'].set_index('Constituents')  # Replace 'Sheet1' with the actual sheet name
-
+eps = dfs['EPS_Q']
+rev = dfs['Rev_Q']
+shr = dfs['Share_Q']
+meta = dfs['meta']
 # Apply the function to each row (excluding the 'Constituents' column)
-eps2 = eps.drop(index=eps.index[np.where(eps.iloc[:,1]=='#INVALID COMPANY ID')[0][0]]).astype('float')
-eps2 = eps2.apply(impute_missing, axis=1).ffill(axis=1).bfill(axis=1)
-rev2 = rev.drop(index=eps.index[np.where(eps.iloc[:,1]=='#INVALID COMPANY ID')[0][0]]).astype('float')
-rev2 = rev2.apply(impute_missing, axis=1).ffill(axis=1).bfill(axis=1)
-shr2 = shr.drop(index=eps.index[np.where(eps.iloc[:,1]=='#INVALID COMPANY ID')[0][0]]).astype('float')
-shr2 = shr2.apply(impute_missing, axis=1).ffill(axis=1).bfill(axis=1)
+eps3 = eps.drop(np.where(eps.iloc[:,1]=='#INVALID COMPANY ID')[0]).set_index('Constituents').astype('float')
+rev3 = rev.drop(np.where(rev.iloc[:,1]=='#INVALID COMPANY ID')[0]).set_index('Constituents').astype('float')
+shr3 = shr.drop(np.where(shr.iloc[:,1]=='#INVALID COMPANY ID')[0]).set_index('Constituents').astype('float')
+meta3 = meta.drop(np.where(meta.iloc[:,1]=='#INVALID COMPANY ID')[0]).set_index('Constituents')
 
+cols = eps3.columns
+_list = []
+for c in cols:
+    e = eps3[c]
+    r = rev3[c]
+    s = shr3[c]
+    tmp = pd.concat([e, r, s], axis=1).dropna()
+    tmp.columns = ['eps', 'revenue', 'share']
+    tmp['net_income'] = tmp['eps'] * tmp['share']
+    tmp['revenue'] = tmp['revenue'] * 1000
+    tmp2 = tmp.sum()
+    c2 = c[3:] + c[1:3]
+    _list.append([c2, tmp2['net_income'], tmp2['revenue'], tmp2['net_income']/tmp2['revenue'], tmp2['share']])
+    for sec in sectors:
+        tickers_in_sector = meta3[meta3['Sector'].eq(sec)]
+        tmp2 = pd.concat([tickers_in_sector, tmp], axis=1).dropna()
+        tmp2 = tmp2.reset_index()
+        tmp2['Quarter'] = c2
+        tmp2['Index'] = 'SP400'
+        output_sec = pd.concat([output_sec, tmp2])
+columns = ['Quarter', 'Earnings', 'Revenue', 'Net Income Margin', 'Shares']
+output2 = pd.DataFrame(_list, columns=columns)
+
+
+################################################################################
+
+# Read the Excel file into a dictionary of dataframes
+file_path = "04_monitoring\JPAM_2025\sp500_earning_sourcce\data\SP600_components.xlsx"  # Replace with your actual file path
+dfs = pd.read_excel(file_path, sheet_name=None, skiprows=2)  # None reads all sheets into a dictionary
+
+# Display the sheet names
+print(dfs.keys())
+
+# Access a specific sheet
+eps = dfs['EPS_Q']
+rev = dfs['Rev_Q']
+shr = dfs['Share_Q']
+meta = dfs['meta']
+# Apply the function to each row (excluding the 'Constituents' column)
+eps4 = eps.drop(np.where(eps.iloc[:,1]=='#INVALID COMPANY ID')[0]).set_index('Constituents').astype('float')
+rev4 = rev.drop(np.where(rev.iloc[:,1]=='#INVALID COMPANY ID')[0]).set_index('Constituents').astype('float')
+shr4 = shr.drop(np.where(shr.iloc[:,1]=='#INVALID COMPANY ID')[0]).set_index('Constituents').astype('float')
+meta4 = meta.drop(np.where(meta.iloc[:,1]=='#INVALID COMPANY ID')[0]).set_index('Constituents')
+
+
+cols = eps3.columns
+_list = []
+for c in cols:
+    e = eps4[c]
+    r = rev4[c]
+    s = shr4[c]
+    tmp = pd.concat([e, r, s], axis=1).dropna()
+    tmp.columns = ['eps', 'revenue', 'share']
+    tmp['net_income'] = tmp['eps'] * tmp['share']
+    tmp['revenue'] = tmp['revenue'] * 1000
+    tmp2 = tmp.sum()
+    c2 = c[3:] + c[1:3]
+    _list.append([c2, tmp2['net_income'], tmp2['revenue'], tmp2['net_income']/tmp2['revenue'], tmp2['share']])
+    for sec in sectors:
+        tickers_in_sector = meta4[meta4['Sector'].eq(sec)]
+        tmp2 = pd.concat([tickers_in_sector, tmp], axis=1).dropna()
+        tmp2 = tmp2.reset_index()
+        tmp2['Quarter'] = c2
+        tmp2['Index'] = 'SP600'
+        output_sec = pd.concat([output_sec, tmp2])
+columns = ['Quarter', 'Earnings', 'Revenue', 'Net Income Margin', 'Shares']
+output3 = pd.DataFrame(_list, columns=columns)
+
+
+with pd.ExcelWriter("04_monitoring\JPAM_2025\sp500_earning_sourcce\SPX_components.xlsx") as writer:
+    output.to_excel(writer, sheet_name='sp500', index=False)
+    output2.to_excel(writer, sheet_name='sp400', index=False)
+    output3.to_excel(writer, sheet_name='sp600', index=False)
+    output_sec.to_excel(writer, sheet_name='total_sector', index=False)
