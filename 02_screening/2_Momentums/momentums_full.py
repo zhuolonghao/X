7698905619703f03fce3fbd17d2402e654531e61
zhuolonghao <103546522+zhuolonghao@users.noreply.github.com>
2025-02-17 20:51:31 -0500

    2.17.2025

--- 02_screening/2_Momentums/momentums_full.py
+++ 02_screening/2_Momentums/momentums_full.py
@@ -2,18 +2,16 @@ import pandas as pd
 import numpy as np
 import os
 folder = f'{os.getcwd()}\\data'
-meta_sp500 = pd.read_excel(f"{folder}\\meta.xlsx", sheet_name='sp500')
-meta_sp500 = meta_sp500.drop(np.where(meta_sp500.iloc[:,1]=='#INVALID COMPANY ID')[0])
-meta_sp400 = pd.read_excel(f"{folder}\\meta.xlsx", sheet_name='sp400')
-meta_sp400 = meta_sp400.drop(np.where(meta_sp400.iloc[:,1]=='#INVALID COMPANY ID')[0])
-meta_sp600 = pd.read_excel(f"{folder}\\meta.xlsx", sheet_name='sp600')
-meta_sp600 = meta_sp600.drop(np.where(meta_sp600.iloc[:,1]=='#INVALID COMPANY ID')[0])
+meta = pd.read_excel(f"{folder}\\meta_full.xlsx", sheet_name='total_market')
+meta = meta.drop(np.where(meta.iloc[:,1]=='#INVALID COMPANY ID')[0])
 
-meta = pd.concat([meta_sp500,meta_sp400, meta_sp600], ignore_index=True)
 meta['Constituents'] = meta['Constituents'].str\
     .replace(r'NasdaqGS', 'NASDAQ', regex=True)\
     .replace(r'NasdaqCM', 'NASDAQ', regex=True)\
-    .replace(r'NasdaqGM', 'NASDAQ', regex=True)\
+    .replace(r'NasdaqGM', 'NASDAQ', regex=True) \
+    .replace(r'NASDAQGS', 'NASDAQ', regex=True) \
+    .replace(r'NASDAQCM', 'NASDAQ', regex=True) \
+    .replace(r'NASDAQGM', 'NASDAQ', regex=True) \
     .replace(r'NYSEAM', 'NYSE', regex=True)
 price = pd.read_csv(f"{folder}\\momentums.csv").drop('Industry', axis=1)
 price['Constituents'] = price['Exchange']+ ':' + price['Symbol']
@@ -29,5 +27,5 @@ df['vol_3m / 1-yr avg vol'] = df['Avg_Daily_vol_over_3m / one-year avg daily_vol
 df['vol / vol_MA_10d'] = df['Relative Volume 1 day'].rank(pct=True)
 df['vol / vol_MA_10w'] = df['Relative Volume 1 week'].rank(pct=True)
 df['vol / vol_MA_10m'] = df['Relative Volume 1 month'].rank(pct=True)
-df.to_excel(f"{os.path.dirname(folder)}\\momentums.xlsx")
+df.to_excel(f"{os.path.dirname(folder)}\\momentums_full.xlsx")
 print("Completed")
