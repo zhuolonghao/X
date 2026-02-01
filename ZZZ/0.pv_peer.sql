60e33f3f7cac4e9908e9abecff1cdc077b644969
Longhao Desktop <longhao.zhuo@gmail.com>
2026-01-31 20:43:57 -0500

    new:

--- ZZZ/0.pv_peer.sql
+++ ZZZ/0.pv_peer.sql
@@ -1,30 +1,22 @@
-/* Teradata SQL SELECT statement for fuzzy matching company names.
-Logic:
-1. Normalization to lowercase.
-2. Stripping of parentheticals and "Public" suffixes.
-3. Use of LIKE '[clean_name]%' for prefix fuzzy matching.
-*/
-
 SELECT
     cust_id,
     cust_nm,
-    segment_nm
+    addr_line_1_txt,
+    city_nm,
+    st_cd
 FROM
-    Customer_Table
+    dw_tables.customer_dimension
 WHERE
-    --- Industrial & Commercial
-    LOWER(cust_nm) LIKE 'texas instruments%'
-    OR LOWER(cust_nm) LIKE 'nxp semiconductors%'
-    OR LOWER(cust_nm) LIKE 'stmicroelectronics%'
-    OR LOWER(cust_nm) LIKE 'infineon technologies%'
-    OR LOWER(cust_nm) LIKE 'renesas electronics%'
-    OR LOWER(cust_nm) LIKE 'microchip technology%'
-    OR LOWER(cust_nm) LIKE 'broadcom%'
-    OR LOWER(cust_nm) LIKE 'qualcomm%'
-   --- Home & Life
-    OR LOWER(cust_nm) LIKE 'nordic semiconductor%'
-    OR LOWER(cust_nm) LIKE 'espressif systems%'
-    OR LOWER(cust_nm) LIKE 'telink semiconductor%'
-    OR LOWER(cust_nm) LIKE 'synaptics%'
-    OR LOWER(cust_nm) LIKE 'mediatek%'
-    OR LOWER(cust_nm) LIKE 'nxp / stmicro / ti%';
+    LOWER(cust_nm) LIKE 'andersen%'
+    OR LOWER(cust_nm) LIKE 'pella%'
+    OR LOWER(cust_nm) LIKE 'marvin%'
+    OR LOWER(cust_nm) LIKE 'masonite%'
+    OR LOWER(cust_nm) LIKE 'therma-tru%'
+    OR LOWER(cust_nm) LIKE 'thermatru%'
+    OR LOWER(cust_nm) LIKE 'ply gem%'
+    OR LOWER(cust_nm) LIKE 'ply-gem%'
+    OR LOWER(cust_nm) LIKE 'milgard%'
+    OR LOWER(cust_nm) LIKE 'owens corning%'
+    OR LOWER(cust_nm) LIKE 'fortune brands%'
+    OR LOWER(cust_nm) LIKE 'builders firstsource%'
+    OR LOWER(cust_nm) LIKE 'builders first source%';
